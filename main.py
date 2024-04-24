9a3d2cb75fb33c4afdb6c986eab3eaf0bec354bc
Kristina Zhuk <sivych26@mail.ru>
2024-04-24 19:50:49 +0300

    qap_18_hw2

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-print("Hello World!!!!")
+dct = [{'name': 'Apple', 'kg': 123, 'date': '12.04.2024'},
+       {'name': 'Banana', 'kg': 200, 'date': '20.04.2024'},
+       {'name': 'Orange', 'kg': 123, 'date': '12.04.2024'}
+]
 
-print("Hello World!!!!")
+print(dct[1].keys())
 
-print("Hello World!!!!")
+red_apple = ({'name': 'Red Apple', 'kg': 200, 'date': '20.04.2024'})
+
+print(dct[1].keys())
+
